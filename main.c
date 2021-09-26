67ee5baa4c3b989e2c0794a9da9d008ebc62ea73
tobias <tobinoyacabj@gmail.com>
2021-09-25 22:44:00 -0300

    creado2

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/// structures
 
+typedef struct
+{
+    char patente[6];
+    int valor;
+    char marca[10];
+} automovil;
+
+typedef struct
+{
+    automovil dato;
+    struct nodoAuto* sig;
+} nodoAuto;
+
+/// funciones
+
+
+
+
+/// main
 
 int main()
 {
